cfdcd6bb6d14fe37bdf8b39eb2b544ab5181202d
stulikova-s <39324320+stulikova-s@users.noreply.github.com>
2019-10-04 05:20:18 +0700

    Add files via upload

---
+++ experiment.py
@@ -0,0 +1,20 @@
+from pprint import pprint
+import requests
+from urllib.parse import urljoin
+from pathlib import Path
+import os
+
+api_token = '0afc96266d8c4cf540413e0aa217493d15577f76'
+username = 'sannydjoun'
+pythonanywhere_host = "www.pythonanywhere.com"
+
+api_base = "https://{pythonanywhere_host}/api/v0/user/{username}/".format(
+    pythonanywhere_host=pythonanywhere_host,
+    username=username,
+)
+
+for root, dirs, files in os.walk("./Work"):
+    for filename in files:
+    	resp = requests.post(urljoin(api_base, 'files/path/home/{username}/steph/templates/zim/{item}'.format(username=username,item=root[2:]+'/'+filename)),
+    		files={"content":open('{item}'.format(item=root[2:]+'/'+filename))},
+    		headers={"Authorization": "Token {api_token}".format(api_token=api_token)})
